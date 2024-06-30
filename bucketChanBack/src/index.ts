7cdbeb871be33fb64f1eab48f42e11f3faee5563
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-06-29 23:12:56 -0400

    cambios en base de datos

--- bucketChanBack/src/index.ts
+++ bucketChanBack/src/index.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { json } from "express";
 import { createServer } from "http";
+import { DatabaseConnection } from "./chan/repository/database.js";
+import { boardRouter } from "./chan/controllers/boardController.js";
 
 const port=3000;
 
 const app=express()
 const server=createServer(app)
+
+app.use(json())
+
+export const dbConnection=await DatabaseConnection.getConnection()
+
+app.use("/board",boardRouter);
 
 app.get("/hello",(req,res)=>{
     res.send("hell");

7fbdf9f5245d79c4579ec96856b2aae51646c99d
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-07-01 17:59:25 -0400

    cors configurar

--- bucketChanBack/src/index.ts
+++ bucketChanBack/src/index.ts
@@ -1,11 +1,16 @@
 import express, { json } from "express";
 import { createServer } from "http";
 import { boardRouter } from "./chan/controllers/boardController.js";
+import cors from "cors"
 
 const port=3000;
 
 const app=express()
 const server=createServer(app)
+
+app.use(cors({
+    origin:"*"
+}))
 
 app.use(json())
 

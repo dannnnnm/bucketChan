3e75a2569c40eaf4c4b71015f34b8c042a48bb50
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-07-02 03:17:16 -0400

    bare bones de chat por websockets

---
+++ bucketChanBack/src/chat/chatMaster.ts
@@ -0,0 +1,100 @@
+import { Server, Socket } from "socket.io";
+import { User } from "../chan/models/User.js";
+import { ChatRoom } from "../chan/models/chat/ChatRoom.js";
+
+const generalRoomName="generalRoom";
+var serverSocket:Server<any, any, any, any>;
+var roomParticipants=new Map<string,{user:User,clientSocket:Socket<any, any, any, any>,isAnon:boolean}[]>()
+
+export async function initSocket(server){
+    serverSocket=new Server(server,{
+        cors:{
+            origin:"*",
+            methods:["GET","POST"]
+        }
+    });
+    let existingChatrooms=await ChatRoom.findAll();
+    existingChatrooms.forEach((chatroom)=>{
+        roomParticipants.set(chatroom.shortName,[]);
+    })
+    roomParticipants.set(generalRoomName,[]);
+    serverSocket.on('connection', (userSocket) => {
+        //por default se une a la sala general
+
+        userSocket.on('joinRoom', async (room, userData) => {
+            
+            userSocket.join(room);
+            let user:User;
+            let isAnon=true;
+            if (!userData.username) return 
+            if (userData.username!="anon"){
+                user=await User.findOne({where:{username:userData.name}})
+                isAnon=false;
+            }
+            else{
+                let anonCount=0
+                roomParticipants.forEach((value,key)=>{
+                    let foundAnons=value.filter(userData=>/anon [0-9]+/.test(userData.user.username));
+                    foundAnons.forEach((anon)=>{
+                        anonCount++;
+                    })
+                })
+                user=new User({
+                    username:`anon ${anonCount+1}`
+                })
+            }
+            roomParticipants.get(room).push({user,clientSocket:userSocket,isAnon})
+            
+            console.log(`${user.username} joined ${room}`)
+            serverSocket.to(room).emit("joinRoom",{message:`${user.username} joined`,author:"system"})
+        })
+
+        userSocket.on('message', (msg) => {
+            let {user,room,found}=findUserAndRoomFromWS(userSocket)
+            serverSocket.to(room).emit('message', {message:msg,author:user.username});
+
+        });
+
+        // Disconnect
+        userSocket.on('disconnect', () => {
+            let found=false;
+            let pair:{user:User,clientSocket:Socket<any, any, any, any>,isAnon:boolean};
+            let eventRoom="";
+            roomParticipants.forEach((value,key)=>{
+                if (found) return;
+                let foundSocket=value.filter(pair=>pair.clientSocket==userSocket);
+                if (foundSocket.length>0){
+                    found=true;
+                    pair=foundSocket[0];
+                    eventRoom=key;
+                }
+            })
+            if (found){
+                serverSocket.to(eventRoom).emit("userDisconnect",{message:`${pair.user.username} has disconnected!`,author:"system"})
+                console.log('user disconnected');
+            }
+            
+        });
+    })
+
+}
+
+function findUserAndRoomFromWS(websocket):{user:User,room:string,pair:any,found:boolean}{
+    let found=false;
+    let user:User;
+    let room:string;
+    let pair:any;
+    roomParticipants.forEach((value,key)=>{
+        if (found) return;
+        let userMatch=value.filter(pair=>pair.clientSocket==websocket);
+        if (userMatch.length>0){
+            found=true;
+            user=userMatch[0].user;
+            room=key;
+            pair=userMatch[0]
+        }
+    })
+    console.log("disco found ",found)
+    return {found,user,room,pair}
+
+}
--- bucketChanBack/src/index.ts
+++ bucketChanBack/src/index.ts
@@ -2,15 +2,14 @@ import express, { json } from "express";
 import { createServer } from "http";
 import { boardRouter } from "./chan/controllers/boardController.js";
 import cors from "cors"
+import { initSocket } from "./chat/chatMaster.js";
 
 const port=3000;
 
 const app=express()
 const server=createServer(app)
 
-app.use(cors({
-    origin:"*"
-}))
+app.use(cors())
 
 app.use(json())
 
@@ -20,6 +19,8 @@ app.use("/board",boardRouter);
 app.get("/hello",(req,res)=>{
     res.send("hell");
 })
+
+initSocket(server);
 
 server.listen(port,"0.0.0.0",()=>{
     console.log("up and running")

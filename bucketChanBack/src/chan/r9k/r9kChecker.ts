ecd8a5432b7beb7cc6c681ba98e815d35be54857
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-07-03 17:31:29 -0400

    r9k debe revisar respuestas a hilos

--- bucketChanBack/src/chan/r9k/r9kChecker.ts
+++ bucketChanBack/src/chan/r9k/r9kChecker.ts
@@ -3,16 +3,24 @@ import { Post } from "../models/imageboard/Post.js";
 
 const r9kStripRegex=/([\s|\W])+/g
 
-export function r9kAllow(boardPosts:Post[],newPost:Post):boolean{
+export function r9kAllow(boardThreads:Post[],newPost:Post):boolean{
     let result=true;
     let modifiedNewPostContent=newPost.body.trim().toLowerCase().replace(r9kStripRegex,"");
-    for (let i = 0; i < boardPosts.length; i++) {
-        const existingPost = boardPosts[i];
+    for (let i = 0; i < boardThreads.length; i++) {
+        const existingPost = boardThreads[i];
         const modifiedExistingPostContent=existingPost.body.trim().toLowerCase().replace(r9kStripRegex,"");
         if (modifiedExistingPostContent==modifiedNewPostContent){
             result=false;
             break;
         }
+        existingPost.responses.forEach((response)=>{
+            if (response.encrypted || result==false) return;
+            const modifiedExistingPostContent=response.body.trim().toLowerCase().replace(r9kStripRegex,"");
+            if (modifiedExistingPostContent==modifiedNewPostContent){
+                result=false;
+                return;
+            }
+        })
         
     }
 

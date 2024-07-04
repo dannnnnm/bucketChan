22c343a8c2d598d191873d4a75643f1b11859d8e
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-07-04 14:41:20 -0400

    fix respuesta de fallo al validar

--- bucketChanBack/src/middleware/validation.ts
+++ bucketChanBack/src/middleware/validation.ts
@@ -3,10 +3,12 @@ import Joi from "joi"
 
 export function validate(validator:Joi.ObjectSchema<any>,subObject:string=null){
     return (req,res,next)=>{
-        let evaluated=subObject!=null?req.body[subObject]:req.body
+        console.log("suboj ",req.body[subObject])
+
+        let evaluated=subObject!=null?JSON.parse(req.body[subObject]):req.body
         let {error}=validator.validate(evaluated)
         if (error){
-            res.status(200).send(error);
+            res.status(400).send(error);
         }
         else{
             next()
